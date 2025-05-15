f51d34a4f072256f411fa7e3b571aec61f617589
Si Thu Zaw <sithuzaw200258@gmail.com>
2025-05-15 14:00:18 +0630

    initial setup

---
+++ src/app/page.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col justify-center items-center">
+      <h1 className="text-4xl font-bold">Hello, Next.js!</h1>
+    </main>
+  );
+}
